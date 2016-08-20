c14e509247b254cc66f693d26e4534453f2637bc
lulalachen <lulalachen@gmail.com>
2016-08-21 04:59:03 +0800

    fix deploy and build

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 echo '😱 😱 😱 Build Start 😱 😱 😱'
 npm run build
 echo '😱 😱 😱 Deployment Start 😱 😱 😱'
-mkdir docs/statics
-cp -rf app/images docs/
-cp -rf app/statics docs/
 git add docs && git commit -m "build gh-page"
 git push origin master
 echo '😱 😱 😱 Deployment Done 😱 😱 😱'
